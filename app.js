27be8f494e6cd691fdc51f5cc8da1a51044ef3e4
ihnatta <annaignatva@gmail.com>
2024-01-08 17:23:16 +0100

    refactor: hange class name and id name rule 1.2 and 3.1, delete &quest rule 2.3, change css rule 3.7,3.6,3.8

--- app.js
+++ app.js
@@ -10,7 +10,7 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("incompleteTasks");//ul of #incompleteTasks
+var incompleteTaskHolder=document.getElementById("incomplete-tasks");//ul of #incompleteTasks
 var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
 
 
@@ -85,7 +85,7 @@ var editTask=function(){
     var editInput=listItem.querySelector('input[type=text]');
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".todo__edit");
-    var containsClass=listItem.classList.contains("todo__list_editMode");
+    var containsClass=listItem.classList.contains("todo__list_edit-mode");
     //If class of the parent is .editmode
     if(containsClass){
 
@@ -99,7 +99,7 @@ var editTask=function(){
     }
 
     //toggle .editmode on the parent.
-    listItem.classList.toggle("todo__list_editMode");
+    listItem.classList.toggle("todo__list_edit-mode");
 };
 
 
